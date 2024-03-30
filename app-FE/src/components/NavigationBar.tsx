e0b2d86cd9c700433e17323fb28d5a905f28439c
n2ai <trinhh7@gator.uhd.edu>
2024-03-29 21:01:43 -0500

    Still working on bugs

--- app-FE/src/components/NavigationBar.tsx
+++ app-FE/src/components/NavigationBar.tsx
@@ -1,27 +1,23 @@
 import { Link } from "react-router-dom"
 import { Icon } from '@iconify/react';
 import React, { ReactHTMLElement, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import '../styles/navigationBar.css'
 
 const NavigationBar:React.FC = ()=>{
-
+    
     const [openNavBar,setOpenNavBar] = useState<boolean>(false)
 
     const handleClick = ()=>{
         setOpenNavBar(prev=>!prev)
     }
 
-    //URL modification function split,add / to 1st elment, and join with the new link
+    const [searchParams, setSearchParams] = useSearchParams()
     
+    const rawUrl = window.location.href.split('?')[0];
+    const homeUrl = rawUrl+`?id=${searchParams.get('id')}`
+    const addClassUrl = rawUrl
     
-    const currentUrl = window.location.href;
-    const urlArray = currentUrl.split('/');
-    const generateUrl = (item:string):string=>{
-        return `${urlArray[0]}//${urlArray[2]}/${urlArray[3]}/${item}/${urlArray[4]}`;
-    }
-
-    const homeUrl = generateUrl('');
-    const addClassUrl = generateUrl('addClass');
     
 
     return(
@@ -30,8 +26,8 @@ const NavigationBar:React.FC = ()=>{
                 <div className="navBar-media">
 
                 </div>
-                <Link className="navBar-link" to={'/'}>Home</Link>
-                <Link className="navBar-link" to={addClassUrl}>Add Class</Link>
+                <Link className="navBar-link" to={homeUrl}>Home</Link>
+                <Link className="navBar-link" to={'/'}>Add Class</Link>
                 <Icon onClick={handleClick} className="navBar-hamburger" icon="ci:hamburger-md" width="2.5rem" height="2.5rem" />
             </div>
             { openNavBar && <div className="navBar-extended">
