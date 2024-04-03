9f32a8f4d633a5867501ac7d4c6f4c51e785fbd6
n2ai <trinhh7@gator.uhd.edu>
2024-04-02 23:00:27 -0500

    Update Link

--- app-FE/src/components/NavigationBar.tsx
+++ app-FE/src/components/NavigationBar.tsx
@@ -1,14 +1,13 @@
 import { Link } from "react-router-dom"
 import { Icon } from '@iconify/react';
 import React, { ReactHTMLElement, useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import '../styles/navigationBar.css'
 
 const NavigationBar:React.FC = ()=>{
     
     const [openNavBar,setOpenNavBar] = useState<boolean>(false)
-    const [searchParams,setSearchParams] = useSearchParams()
-    const id = searchParams.get('id')
+    const {id} = useParams()
     console.log(id)
     const handleClick = ()=>{
         setOpenNavBar(prev=>!prev)
@@ -18,14 +17,16 @@ const NavigationBar:React.FC = ()=>{
         <div className="navBar-main">
             <div className="navBar">
                 <div className="navBar-media">
-
+    
                 </div>
-                <Link className="navBar-link" to={`/profile?id=${id}`}>Home</Link>
-                <Link className="navBar-link" to={`/profile/addClass?id=${id}`}>Add Class</Link>
+                <Link className="navBar-link" to={`/profile/${id}`}>Home</Link>
+                <Link className="navBar-link" to={`/profile/${id}/addClass}`}>Add Class</Link>
+                <Icon onClick={handleClick} className="navBar-hamburger" icon="ci:hamburger-md" width="2.5rem" height="2.5rem" />
+
             </div>
             { openNavBar && <div className="navBar-extended">
-                <Link className="navBar-extended-link" to={`/profile?id=${id}`}>Home</Link>
-                <Link className="navBar-extended-link" to={`/profile/addClass?id=${id}`}>Add Class</Link>
+                <Link className="navBar-extended-link" to={`/profile/${id}}`}>Home</Link>
+                <Link className="navBar-extended-link" to={`/profile/${id}/addClass}`}>Add Class</Link>
                 
             </div>}
         </div>
--- app-FE/src/pages/HomePage.tsx
+++ app-FE/src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, ReactElement } from "react";
 import axios, { AxiosResponse } from "axios";
 import { Link, useNavigate, useParams, useSearchParams } from "react-router-dom";
+import '../styles/homePage.css';
 
 interface IEnrollments{
     EnrollmentId:number,
@@ -14,6 +15,21 @@ const HomePage:React.FC = ()=>{
     //Get data based on params id
     const params = useParams()
     // const navigate = useNavigate()
+
+    //Convert date function:
+    const convertDate = (timestamp:string):string=>{
+        const date:Date = new Date(timestamp);
+
+        const year: number = date.getFullYear();
+
+        const month: string = ('0' + (date.getMonth() + 1)).slice(-2);
+
+        const day: string = ('0' + date.getDate()).slice(-2);
+
+        const formattedDate: string = `${month}-${day}-${year}`;
+
+        return formattedDate
+    }
 
     const [userEnrollments, setUserEnrollments] = useState<IEnrollments[]>([{
         EnrollmentId:0,
@@ -41,19 +57,23 @@ const HomePage:React.FC = ()=>{
     },[params.id]);
 
     const tableData = userEnrollments.map((item:IEnrollments)=>{
-        <tr>
+        return (<tr key={item.ClassId}>
             <td>{item.EnrollmentId}</td>
             <td>{item.UserId}</td>
             <td>{item.ClassId}</td>
             <td>{item.ClassCondition}</td>
-            <td>{item.EnrollmentDate}</td>
-        </tr>
+            <td>{convertDate(item.EnrollmentDate)}</td>
+        </tr>)
     })
 
     return(
         <div>
             <div>
                 {/**Title Session */}
+                <h1>My Class</h1>
+                <div>
+                    Search bar section
+                </div>
             </div>
 
             <div>
@@ -66,8 +86,7 @@ const HomePage:React.FC = ()=>{
                         <th>Class Condition</th>
                         <th>Enrollment Date</th>
                     </tr>
-
-
+                    {tableData}
                 </table>
             </div>
         </div>
