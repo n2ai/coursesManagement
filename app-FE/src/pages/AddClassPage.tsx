93ef60b9f5de8f3ff63ae4a0ca20b6ccd71ae39f
n2ai <trinhh7@gator.uhd.edu>
2024-04-03 22:02:22 -0500

    Add Table for course Catalouge

--- app-FE/src/pages/AddClassPage.tsx
+++ app-FE/src/pages/AddClassPage.tsx
@@ -1,7 +1,37 @@
+import {Paper, Table, TableHead, TableRow, TableContainer, TableCell} from "@mui/material"
 const AddClassPage:React.FC = ()=>{
+
+    const columns = [
+        {id:'Class Id',name:'Class Id'},
+        {id:'Class Name', name:'Class Name'},
+        {id:'Instructor', name:'Instructor'},
+        {id:'Room', name:'Room'},
+        {id:'Credit', name:'Credit'}
+    ]
+
     return(
         <div>
-            This is Add Class page
+            <h1>
+                Class Catalouge
+            </h1>
+
+            <Paper >
+                <TableContainer>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                {columns.map((item)=>{
+                                    return (
+                                    <TableCell id={item.id}>
+                                        {item.name}
+                                    </TableCell>
+                                    )
+                                })}
+                            </TableRow>
+                        </TableHead>
+                    </Table>
+                </TableContainer>
+            </Paper>
         </div>
     )
 };
